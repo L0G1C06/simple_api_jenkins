92488acd1d7182b3920509fb1b9337e4c3386d9e
L0G1C06 <justgithub70@gmail.com>
2024-02-14 16:57:49 -0300

    solve error to run docker container with python

--- exec_jenkins.sh
+++ exec_jenkins.sh
@@ -1,1 +1,1 @@
-docker run -u 0 -d -p 8080:8080 -p 50000:50000 -v /data/jenkins:/var/jenkins_home jenkins/jenkins
+docker run -u 0 --privileged -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v /data/jenkins:/var/jenkins_home jenkins/jenkins
